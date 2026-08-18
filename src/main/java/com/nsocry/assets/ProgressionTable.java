a48dbb71039b2e050828af31fb5f8fae12dc82ff
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-08-18 18:36:50 +0700

    feat: thêm DATA asset container

---
+++ src/main/java/com/nsocry/assets/ProgressionTable.java
@@ -0,0 +1,15 @@
+package com.nsocry.assets;
+
+/** Các bảng số nguyên progression được gửi cho client theo thứ tự cố định. */
+public enum ProgressionTable {
+    CRYSTAL_REQUIREMENT,
+    CLOTHE_REQUIREMENT,
+    ADORN_REQUIREMENT,
+    WEAPON_REQUIREMENT,
+    CRYSTAL_COIN_COST,
+    CLOTHE_COIN_COST,
+    ADORN_COIN_COST,
+    WEAPON_COIN_COST,
+    GOLD_COST,
+    MAX_PERCENT
+}
