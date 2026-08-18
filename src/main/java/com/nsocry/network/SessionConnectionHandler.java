f19b0be10f28f5e10a3f65d1e76ebee3dc1bf6de
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-08-18 17:05:30 +0700

    docs: explain SessionConnectionHandler

--- src/main/java/com/nsocry/network/SessionConnectionHandler.java
+++ src/main/java/com/nsocry/network/SessionConnectionHandler.java
@@ -3,6 +3,7 @@ package com.nsocry.network;
 import java.net.Socket;
 
 @FunctionalInterface
+/** Application boundary invoked once for every socket accepted by {@link TcpServer}. */
 public interface SessionConnectionHandler {
     void handle(Socket socket) throws Exception;
 }
