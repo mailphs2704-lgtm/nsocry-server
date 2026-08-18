fa4072476f984c698a1014410d49a25feeac22ce
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-08-18 17:19:31 +0700

    feat: thêm package-info.java

---
+++ src/main/java/com/nsocry/bootstrap/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Ghép dependency và quản lý vòng đời tiến trình NSOCry.
+ *
+ * <p>Package này chỉ composition; không được chứa logic protocol, database hoặc gameplay.</p>
+ */
+package com.nsocry.bootstrap;
