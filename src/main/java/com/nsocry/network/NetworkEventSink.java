8b1d87ea5b16757bb8814cc2b87681adc49e4e30
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-08-18 17:05:27 +0700

    docs: explain NetworkEventSink

--- src/main/java/com/nsocry/network/NetworkEventSink.java
+++ src/main/java/com/nsocry/network/NetworkEventSink.java
@@ -3,6 +3,7 @@ package com.nsocry.network;
 import java.io.IOException;
 import java.net.SocketAddress;
 
+/** Receives sanitized listener and session lifecycle failures without coupling networking to a logger. */
 public interface NetworkEventSink {
     void sessionFailed(SocketAddress remoteAddress, Exception failure);
 
