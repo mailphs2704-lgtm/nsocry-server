544158b11cea908c1b8568f0ccb90e9c89293e44
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-08-18 17:04:57 +0700

    docs: document session package

---
+++ src/main/java/com/nsocry/session/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Defines connection bootstrap state, client handshake decoding and ports used
+ * to obtain session keys and authenticate accounts.
+ *
+ * <p>The package coordinates protocol behavior without depending on persistence.</p>
+ */
+package com.nsocry.session;
