4612e80dc8728869c767e77a85bbad6fad7cd8ba
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-08-18 17:19:27 +0700

    feat: thêm package-info.java

---
+++ src/main/java/com/nsocry/observability/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Cung cấp các bộ điều hợp quan sát runtime đã làm sạch dữ liệu trước khi ghi log.
+ *
+ * <p>Package này không được ghi password, token, payload client hoặc exception message không tin cậy.</p>
+ */
+package com.nsocry.observability;
